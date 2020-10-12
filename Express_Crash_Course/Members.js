c96c60f8471899fc485c3f796f55291ff355765e
besi <besalelu.php@gmail.com>
2020-10-12 19:27:52 +0530

    Course Completed

---
+++ Express_Crash_Course/Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john.doe@email.com',
+        status: 'Active'
+    },
+    {
+        id: 2,
+        name: 'Will Smith',
+        email: 'will.smith@email.com',
+        status: 'Active'
+    },
+    {
+        id: 3,
+        name: 'Bob William',
+        email: 'bob.william@email.com',
+        status: 'Inactive'
+    }
+];
+
+module.exports = members;
